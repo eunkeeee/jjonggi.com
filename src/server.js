fef5c04d2c4ae1f1d355b8cea04d877029505bf2
Eunkeee <eunkeeeee@gmail.com>
2022-10-16 19:35:09 +0900

    Routes 만들기

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 4000;
 
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("hi"));
+app.get("/login", (req, res) => res.send("Login"));
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 });
