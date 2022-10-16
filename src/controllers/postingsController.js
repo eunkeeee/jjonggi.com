d8bd5259918ebc49c32cfd252c2d69a9af99d625
Eunkeee <eunkeeeee@gmail.com>
2022-10-16 21:51:09 +0900

    각각 포스팅

--- src/controllers/postingsController.js
+++ src/controllers/postingsController.js
@@ -1,9 +1,10 @@
+let postings = [
+  { id: 1, title: "은기의 브이로그", contents: "아무것도 없음" },
+  { id: 2, title: "지훈이의 브이로그", contents: "아직 아무것도 없음" },
+  { id: 3, title: "초키의 브이로그", contents: "아무것도 없음" },
+];
+
 export const showMainPostings = (req, res) => {
-  const postings = [
-    { id: 1, title: "은기의 브이로그", contents: "아무것도 없음" },
-    { id: 2, title: "지훈이의 브이로그", contents: "아직 아무것도 없음" },
-    { id: 3, title: "초키의 브이로그", contents: "아무것도 없음" },
-  ];
   return res.render("home", { pageTitle: "메인", postings });
 };
 export const search = (req, res) => res.send("Search");
@@ -12,7 +13,11 @@ export const showPosting = (req, res) => {
   const {
     params: { id },
   } = req;
-  return res.render("posting");
+  const posting = postings[id - 1];
+  return res.render("showPosting", {
+    pageTitle: `${id}번 포스팅 보는중`,
+    posting,
+  });
 };
 export const edit = (req, res) => res.render("edit");
 export const deletePosting = (req, res) => res.send("deletePosting");
