0188d2d374ba13df8073d0c8628d04748a5d114c
Eunkeee <eunkeeeee@gmail.com>
2022-10-16 21:35:00 +0900

    Show Postings 작은 array로 만듦

--- src/controllers/postingsController.js
+++ src/controllers/postingsController.js
@@ -1,4 +1,11 @@
-export const showMainPostings = (req, res) => res.render("home");
+export const showMainPostings = (req, res) => {
+  const postings = [
+    { id: 1, title: "은기의 브이로그", contents: "아무것도 없음" },
+    { id: 2, title: "지훈이의 브이로그", contents: "아직 아무것도 없음" },
+    { id: 3, title: "초키의 브이로그", contents: "아무것도 없음" },
+  ];
+  return res.render("home", { pageTitle: "메인", postings });
+};
 export const search = (req, res) => res.send("Search");
 
 export const showPosting = (req, res) => {
