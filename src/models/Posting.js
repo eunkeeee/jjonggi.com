d3a1e67888eba97bd7168df91b985dd276b22e99
Eunkeee <eunkeeeee@gmail.com>
2022-10-17 11:47:29 +0900

    Posting Schema 만듦

---
+++ src/models/Posting.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const PostingSchema = new mongoose.Schema({
+  caption: String,
+  tagPeople: [{ type: String }],
+  addLocation: String,
+  hashtags: [{ type: String }],
+  createdAt: { type: Date, required: true, default: Date.now },
+  meta: {
+    likes: { type: Number, default: 0, required: true },
+    savedtoCollections: { type: Number, default: 0, required: true },
+  },
+  owner: String,
+});
+
+const Posting = mongoose.model("Posting", PostingSchema);
+
+export default Posting;
