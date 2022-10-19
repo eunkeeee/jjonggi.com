b59a93930e735cacb4052c0eee48a4805ce3c76f
Eunkeee <eunkeeeee@gmail.com>
2022-10-19 12:46:23 +0900

    회원가입 + hashing pw + pw strength 버그생겨서 패키지로 대체

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,16 @@
-import { checkPasswordStrength } from "../checkPasswordStrength";
+import { passwordStrength } from "check-password-strength";
+
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, username, password, password2 },
   } = req;
+
   // 1. pw vs pw2
   if (password !== password2) {
     return res.render("join", {
@@ -16,14 +19,31 @@ export const postJoin = (req, res) => {
     });
   }
   // 2. pw strength checker
-  if (!checkPasswordStrength(password)) {
+  if (passwordStrength(password).id <= 0) {
     return res.render("join", {
       pageTitle: "Join",
       errorMessage:
-        "Your password must contain at least 8 characters with and numeric character.",
+        "Your password must contain at least 6 characters with uppercase, and number.",
     });
   }
+
   // 3. email, username 중복여부 체크
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken",
+    });
+  }
+
+  // 다 통과했으면 회원가입
+  await User.create({
+    email,
+    username,
+    password,
+    name,
+  });
+  return res.redirect("/login");
 };
 
 export const login = (req, res) => res.send("Login");
--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+});
+
+// user 저장 전에 pw hashing
+userSchema.pre("save", async function () {
+  console.log("user password:", this.password);
+  this.password = await bcrypt.hash(this.password, (saltRounds = 5));
+  console.log("hashed password:", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
