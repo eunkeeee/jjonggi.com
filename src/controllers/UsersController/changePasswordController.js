a942df0d9a91cb96c6aee8beb1491e5500ed6a45
Eunkeee <eunkeeeee@gmail.com>
2022-10-19 20:45:57 +0900

    pw 수정기능 개선

--- src/controllers/UsersController/changePasswordController.js
+++ src/controllers/UsersController/changePasswordController.js
@@ -13,29 +13,38 @@ export const postChangePassword = async (req, res) => {
     body: { currentPassword, newPassword, newPassword2 },
   } = req;
   const user = await User.findOne({ _id });
+
+  // pw 두번 적은거 같게 적어야함
   if (newPassword !== newPassword2) {
     return res.status(400).render("users/change-password", {
       pageTitle: "Change Password",
       errorMessage: "Password confirmation does not match!",
     });
   }
-  console.log("PW 두개는 서로 같음");
+
+  // 과거 비번이랑 바뀌어야 함
+  const isSame = await bcrypt.compare(newPassword, user.password);
+  if (isSame) {
+    return res.status(400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "Please change the password!",
+    });
+  }
+
   // 기존 pw이 맞는지 확인
   const correctCurrentPassword = await bcrypt.compare(
     currentPassword,
     user.password
   );
-  console.log("내가 적은거:", currentPassword);
-  console.log("DB에 저장된거:", user.password);
   if (!correctCurrentPassword) {
     return res.status(500).render("users/change-password", {
       pageTitle: "Change Password",
       errorMessage: "Please write correct current password!",
     });
   }
+
   // 새로운 pw를 저장하기
-  console.log("OLD: ", user.password);
   user.password = newPassword;
   await user.save(); // save를 해야 pre save fn이 실행되며 pw가 hashing되어 저장됨
-  return res.redirect("/");
+  return res.redirect("/users/logout");
 };
